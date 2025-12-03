3dfbe3158dbc69ec711c2ccd98a3b426ed181a7f
humblecode07 <123628072+humblecode07@users.noreply.github.com>
2025-12-03 17:44:59 +0800

    refactor: remove unused App.css import

--- clientSide/src/App.tsx
+++ clientSide/src/App.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './styles/App.css'
 import './styles/Components/Header/Header.css'
 import './styles/Pages/Home.css'
 import './styles/Components/Footer/Footer.css'
