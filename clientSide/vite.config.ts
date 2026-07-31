20e3ba875017cd2bc4738629c417d61eb09392c6
humblecode07 <calderonmiko011@gmail.com>
2026-07-31 21:58:21 +0800

    Add sitemap and robots.txt for SEO

--- clientSide/vite.config.ts
+++ clientSide/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://tskr-movie-database.vercel.app",
+    }),],
   server: {
     hmr: true, // Ensure this is not set to false
   }
